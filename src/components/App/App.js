2fb68fcf4608119deaae8b6c630b2f05bc2ccb3f
Cyrillus <chenetcyril@free.fr>
2022-10-03 14:33:53 +0200

    test2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import {
-  // Route,
+  Route,
   Routes,
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Footer from '../Footer/Footer';
 import Nav from '../Nav/Nav';
-// import EspacePerso from '../EspacePerso/EspacePerso';
+import EspacePerso from '../EspacePerso/EspacePerso';
 import './App.scss';
 import ModalAlertConnection from '../ModalAlertConnection/ModalAlertConnection';
+import EspaceCoworker from '../EspacePerso/EspaceCoworker/EspaceCoworker';
 
 function App() {
   // On vérifie si le token n'a pas expiré en récupérant l'état de connexion
@@ -16,10 +17,11 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <EspaceCoworker />
       {/* Si le token a expiré, on récupère une erreur et donc on incite l'user à se reconnecter */}
       {errorConnection && <ModalAlertConnection />}
       <Routes>
-        {/* <Route path="/espace-perso" element={<EspacePerso />} /> */}
+        <Route path="/espace-perso" element={<EspacePerso />} />
       </Routes>
       <Footer />
     </div>
